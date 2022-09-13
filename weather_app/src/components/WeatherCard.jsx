1cdf68c0d54fdd5c2e18eed4249160ffc8ab3165
Mauricio-Rubio <mau.r.h54@gmail.com>
2022-09-13 18:39:59 -0500

    Font weather card update

--- weather_app/src/components/WeatherCard.jsx
+++ weather_app/src/components/WeatherCard.jsx
@@ -4,10 +4,10 @@ export default function WeatherCard(props) {
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
             {/* <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains" /> */}
-            <div className="font-bold text-xl mb-2">{props.text.country}</div>
+            <div className="text-teal-700 font-bold text-xl mb-2">{props.text.country}</div>
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{props.text.name}</div>
-                <p className="text-gray-700 text-base">
+                <div className="text-teal-500 font-bold text-xl mb-2">{props.text.name}</div>
+                <p className="text-gray-400 text-base font-bold">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                 </p>
             </div>
