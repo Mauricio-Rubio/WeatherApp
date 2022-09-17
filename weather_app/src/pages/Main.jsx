ea5a8cc311477aabcc342c28a4c5e572cde2ddf5
Mauricio-Rubio <mau.r.h54@gmail.com>
2022-09-17 14:58:30 -0500

    Button input file margin adjust

--- weather_app/src/pages/Main.jsx
+++ weather_app/src/pages/Main.jsx
@@ -80,9 +80,7 @@ function Main(props) {
             }
         }
         setCities(ctAux2);
-        // console.log(iata);
-        // console.log(ctAux2);
-        // setInfoConsult(ctAux2);
+        setInfoConsult(ctAux2);
     }
 
     function saveCities(params) {
@@ -98,7 +96,6 @@ function Main(props) {
         return [...dataArr];
     }
 
-    // console.log(getKey());
     return (
 
         <div className="grid grid-cols-1 h-screen w-full">
@@ -109,7 +106,7 @@ function Main(props) {
                         <label htmlFor="formFileSm" className="form-label inline-block mb-2 text-gray-700">Enter a csv file</label>
                         <input accept=".csv" className="form-control block w-full px-2 py-1 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="formFileSm" type="file" onChange={(e) => uploadFile(e.target.files)} />
                         <div className="flex space-x-2 justify-center">
-                            <button onClick={() => { startReading() }} type="button" className="inline-block px-6 py-2.5 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-teal-500/40 hover:shadow-lg focus:bg-teal-500/40 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-teal-500/40 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                            <button onClick={() => { startReading() }} type="button" className="inline-block px-6 py-2.5 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-teal-500/40 hover:shadow-lg focus:bg-teal-500/40 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-teal-500/40 active:shadow-lg transition duration-150 ease-in-out mt-4">Button</button>
                         </div>
                     </div>
                 </div>
